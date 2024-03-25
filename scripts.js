ed72ba6fae98221195f7af1c51a16ebfb3448a39
Steve Sajan Jacob <stevesajanjacobkallunkal04@gmail.com>
2024-03-26 01:37:16 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#type-desc', {
+    strings: ['an Entrepreneur', 'a Developer' , 'a Designer' , 'Google Analytics Expert', 'a Google Certified Digital Marketing Expert'],
+    typeSpeed: 150,
+    backspeed: 50,
+    loop : true
+  });
